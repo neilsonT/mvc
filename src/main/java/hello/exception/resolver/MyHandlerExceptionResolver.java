82809d9162c6d41de1cfa724798fb57b788e3882
taehyun <taehyun16@naver.com>
2021-07-21 21:52:04 +0900

    Initial commit

---
+++ src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,32 @@
+package hello.exception.resolver;
+
+import com.sun.net.httpserver.HttpServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        log.info("ExceptionResolver");
+
+        try {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                return new ModelAndView();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
